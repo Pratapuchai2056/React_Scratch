aa44a5a35cdff01933d83ea212f19aedf29c7819
Tap <125111344+Pratapuchai2056@users.noreply.github.com>
2024-08-08 13:30:28 +0000

    counter app and usestate

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,36 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+const [counter, setCounter] = useState(0)
+
+// let counter = 10
+
+const addValue = () => {
+  // counter = counter + 1  
+  if (counter < 200) {
+    setCounter(prevCounter => prevCounter + 1)
+  }
+};
+const removeValue = () => { 
+  if (counter > 0) {
+    setCounter(prevCounter => prevCounter - 1)
+  }
+};
+
+  return (
+    <>
+     <h1>Tap is Funny</h1>
+     <h2>Counter value: {counter}</h2>
+
+     <button onClick ={addValue}>Add value: +1</button>
+     <br />
+     <button onClick ={removeValue}>Remove value: -1</button>
+     <p>Count: {counter}</p>
+    </>
+  )
+}
+
+export default App
